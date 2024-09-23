677b818c63ea0215966553d7a3ee5660c2de2394
Tatyana Kondratyeva <tatyana.k@gmx.net>
2024-09-23 20:27:08 +0200

    changed project Name

---
+++ src/main/java/de/ait/smallBusiness_be/SmallBusinessBeApplication.java
@@ -0,0 +1,13 @@
+package de.ait.smallBusiness_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallBusinessBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallBusinessBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ait/smallBusiness_be/SmallBusinessBeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ait.smallBusiness_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallBusinessBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
