bc721c2ef9f8023bd8ac1a1213c08263778d37eb
GalinaChechkina <galina.chechkina@gmail.com>
2024-11-25 11:40:49 +0100

    added field artName in ProductCategory

--- src/main/java/de/ait/smallBusiness_be/SmallBusinessBeApplication.java
+++ src/main/java/de/ait/smallBusiness_be/SmallBusinessBeApplication.java
@@ -1,13 +1,22 @@
 package de.ait.smallBusiness_be;
 
+import de.ait.smallBusiness_be.products.model.Dimensions;
+import de.ait.smallBusiness_be.products.model.Product;
+import de.ait.smallBusiness_be.products.model.ProductCategory;
+import de.ait.smallBusiness_be.products.model.UnitOfMeasurement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class SmallBusinessBeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SmallBusinessBeApplication.class, args);
+        Product product = new Product(1L, "product", "EL-6", "qwer", BigDecimal.valueOf(10.10), BigDecimal.valueOf(15.10), UnitOfMeasurement.KG, BigDecimal.valueOf(5.0), new Dimensions(BigDecimal.valueOf(5.000), BigDecimal.valueOf(5.0), BigDecimal.valueOf(5.0)),new ProductCategory(1, "COMPUTER & ZUBEHÖR","COM"),"drtfhdsfhd","345", LocalDate.now(), LocalDateTime.now(), LocalDateTime.now());
+        System.out.println(product);
     }
-
 }
--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,12 +1,14 @@
-INSERT INTO product_categories (name)
-VALUES ('COMPUTER & ZUBEHÖR');
-INSERT INTO product_categories (name)
-VALUES ('BELEUCHTUNG & LED');
-INSERT INTO product_categories (name)
-VALUES ('ENERGIE & AKKUS');
-INSERT INTO product_categories (name)
-VALUES ('SMART HOME');
-INSERT INTO product_categories (name)
-VALUES ('SMARTPHONES & TELEFONE');
-INSERT INTO product_categories (name)
-VALUES ('NETZWERK & INTERNET');
+INSERT INTO product_categories (name, art_name)
+VALUES ('COMPUTER & ZUBEHÖR','COM');
+INSERT INTO product_categories (name, art_name)
+VALUES ('BELEUCHTUNG & LED','BL');
+INSERT INTO product_categories (name, art_name)
+VALUES ('ENERGIE & AKKUS','AKK');
+INSERT INTO product_categories (name, art_name)
+VALUES ('SMART HOME','SH');
+INSERT INTO product_categories (name, art_name)
+VALUES ('SMARTPHONES & TELEFONE','TEL');
+INSERT INTO product_categories (name, art_name)
+VALUES ('NETZWERK & INTERNET','NETZ');
+INSERT INTO product_categories (name, art_name)
+VALUES ('ELEKTRIK','EL');
