d003507942e9f152ce02b3e89bacf47ab3f22297
GalinaChechkina <galina.chechkina@gmail.com>
2024-12-04 13:24:40 +0100

    added @Schema

--- src/main/java/de/ait/smallBusiness_be/products/dto/NewProductDto.java
+++ src/main/java/de/ait/smallBusiness_be/products/dto/NewProductDto.java
@@ -1,6 +1,7 @@
 package de.ait.smallBusiness_be.products.dto;
 
 import de.ait.smallBusiness_be.products.model.ProductCategory;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -22,15 +23,23 @@ public class NewProductDto{
 
     @NotBlank(message = "{validation.notBlank}")
     @Size(min = 3, max = 100, message = "{validation.name.size}")
+    @Schema(description = "Product's name", example = "Lampe")
     String name;
 
     @Size(min = 3, max = 50, message = "{validation.name.size}")
+    @Schema(description = "Product's vendor article", example = "ab123")
     String vendorArticle;
 
     @DecimalMin(value = "0.0", message = "{validation.price.min}")
     @Digits(integer = 10, fraction = 2, message = "{validation.price.digits}")
+    @Schema(description = "Product's purchasing price", example = "20.0")
     BigDecimal purchasingPrice;
 
     @NotNull(message = "{validation.notNull}")
+    @Schema(description = "Product's category", example = "{\n" +
+            "    \"id\": 7,\n" +
+            "    \"name\": \"ELEKTRIK\",\n" +
+            "    \"artName\": \"EL\"\n" +
+            "  }")
     ProductCategory productCategory;
 }
