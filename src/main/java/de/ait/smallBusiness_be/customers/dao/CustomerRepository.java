6065d04226b13e66050c0d1f711d6ed8f4d4c766
GalinaChechkina <galina.chechkina@gmail.com>
2025-01-09 22:01:10 +0100

    added methods: findByCustomerNumber, findAllByCustomerNumberIsNotNull

--- src/main/java/de/ait/smallBusiness_be/customers/dao/CustomerRepository.java
+++ src/main/java/de/ait/smallBusiness_be/customers/dao/CustomerRepository.java
@@ -2,9 +2,14 @@ package de.ait.smallBusiness_be.customers.dao;
 
 import de.ait.smallBusiness_be.customers.model.Address;
 import de.ait.smallBusiness_be.customers.model.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * @author admin
@@ -13,4 +18,8 @@ import java.util.Optional;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     Optional<Customer> findByNameAndAddress(String name, Address address);
+
+    Optional<Customer> findByCustomerNumber(String customerNumber);
+
+    Page<Customer> findAllByCustomerNumberIsNotNull(Pageable pageable);
 }
