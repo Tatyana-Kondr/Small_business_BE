2d38f2b8c3fc2dbdc3d41a0b7c40f9f3aaedfe91
GalinaChechkina <galina.chechkina@gmail.com>
2025-01-09 22:05:54 +0100

    set up a mapper for the address

--- src/main/java/de/ait/smallBusiness_be/configuration/ServiceConfiguration.java
+++ src/main/java/de/ait/smallBusiness_be/configuration/ServiceConfiguration.java
@@ -1,6 +1,9 @@
 package de.ait.smallBusiness_be.configuration;
 
 
+import de.ait.smallBusiness_be.customers.dto.CustomerDto;
+import de.ait.smallBusiness_be.customers.dto.NewCustomerDto;
+import de.ait.smallBusiness_be.customers.model.Customer;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
@@ -22,6 +25,18 @@ public class ServiceConfiguration {
         modelMapper.getConfiguration().setFieldMatchingEnabled(true)
                 .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
                 .setMatchingStrategy(MatchingStrategies.STRICT);
+
+        // Настройка маппинга Customer -> CustomerDto
+        modelMapper.createTypeMap(Customer.class, CustomerDto.class)
+                .addMapping(Customer::getAddress, CustomerDto::setAddress);
+
+        // Настройка маппинга CustomerDto -> Customer
+        modelMapper.createTypeMap(CustomerDto.class, Customer.class)
+                .addMapping(CustomerDto::getAddress, Customer::setAddress);
+
+        // Настройка маппинга NewCustomerDto -> Customer
+        modelMapper.createTypeMap(NewCustomerDto.class, Customer.class)
+                .addMapping(NewCustomerDto::getAddressDto, Customer::setAddress);
         return modelMapper;
     }
 }
