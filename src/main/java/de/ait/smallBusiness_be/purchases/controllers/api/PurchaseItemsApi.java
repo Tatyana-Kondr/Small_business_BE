31448511af82d5a21870989c3a3c13bbb540f4eb
Tatyana Kondratyeva <tatyana.k@gmx.net>
2025-01-20 09:31:42 +0100

    add PurchasesApi and PurchaseItemsApi

---
+++ src/main/java/de/ait/smallBusiness_be/purchases/controllers/api/PurchaseItemsApi.java
@@ -0,0 +1,156 @@
+package de.ait.smallBusiness_be.purchases.controllers.api;
+
+import de.ait.smallBusiness_be.customers.dto.CustomerDto;
+import de.ait.smallBusiness_be.exceptions.ErrorResponseDto;
+import de.ait.smallBusiness_be.purchases.dto.NewPurchaseDto;
+import de.ait.smallBusiness_be.purchases.dto.NewPurchaseItemDto;
+import de.ait.smallBusiness_be.purchases.dto.PurchaseDto;
+import de.ait.smallBusiness_be.purchases.dto.PurchaseItemDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@Tags(
+        @Tag(name = "PurchaseItems")
+)
+@RequestMapping("/api/purchaseItems")
+public interface PurchaseItemsApi {
+
+    //@PreAuthorize("isAuthenticated()")
+    @PostMapping
+    @Operation(
+            summary = "Add a new purchase item",
+            description = "Create a new purchase item. Admin is allowed.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Purchase item created successfully.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PurchaseDto.class))),
+            @ApiResponse(responseCode = "400",
+                    description = "Invalid purchase item data.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class))),
+
+            @ApiResponse(responseCode = "401",
+                    description = "User unauthorized.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(type = "string"))),
+            @ApiResponse(responseCode = "403",
+                    description = "Forbidden.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(type = "string")))
+    })
+    @ResponseStatus(HttpStatus.CREATED)
+    PurchaseItemDto createPurchaseItem(
+            @RequestBody @Valid NewPurchaseItemDto newPurchaseItemDto);
+
+    //@PreAuthorize("isAuthenticated()")
+    @GetMapping("/{purchaseId}")
+    @Operation(
+            summary = "Get all items of purchase by purchase_ID ",
+            description = "Retrieve a list of all items of purchase.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "List of Purchase's items retrieved successfully.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PurchaseItemDto[].class))),
+            @ApiResponse(responseCode = "404",
+                    description = "No items found for purchase with this id.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(responseCode = "401",
+                    description = "User unauthorized.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(type = "string")))
+    })
+    @ResponseStatus(HttpStatus.OK)
+    Page<PurchaseItemDto> getAllPurchaseItemsByPurchaseId(
+            @PageableDefault(size = 10) Pageable pageable,
+            @RequestParam(defaultValue = "name") String sort,
+            @PathVariable Long purchaseId);
+
+    @GetMapping("/{id}")
+    @Operation(
+            summary = "Get purchase item by ID",
+            description = "Retrieve a purchase item by its ID. Allowed to all users.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Purchase item retrieved successfully.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PurchaseDto.class))),
+            @ApiResponse(responseCode = "404",
+                    description = "purchase item not found.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
+    @ResponseStatus(HttpStatus.OK)
+    PurchaseItemDto getPurchaseItemById(@PathVariable Long id);
+
+
+
+    //@PreAuthorize("isAuthenticated()")
+    @PutMapping("/{id}")
+    @Operation(
+            summary = "Update the purchase item",
+            description = "Update the purchase item. Admin is allowed.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Purchase item updated successfully.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PurchaseDto.class))),
+            @ApiResponse(responseCode = "400",
+                    description = "Invalid purchase item data.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(responseCode = "401",
+                    description = "User unauthorized.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(type = "string"))),
+            @ApiResponse(responseCode = "403",
+                    description = "Forbidden.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(type = "string")))
+    })
+    @ResponseStatus(HttpStatus.OK)
+    PurchaseItemDto updatePurchaseItem(
+            @PathVariable Long id,
+            @RequestBody @Valid NewPurchaseItemDto newPurchaseItemDto);
+
+    //@PreAuthorize("isAuthenticated()")
+    @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Delete purchase item by ID",
+            description = "Delete an existing purchase item. Admin is allowed.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",
+                    description = "Purchase item deleted successfully."),
+            @ApiResponse(responseCode = "401",
+                    description = "User unauthorized.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "403",
+                    description = "Forbidden.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(responseCode = "404",
+                    description = "Purchase item not found.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deletePurchaseItem(
+            @PathVariable Long id);
+
+}
---
+++ src/main/java/de/ait/smallBusiness_be/purchases/controllers/api/PurchasesApi.java
@@ -0,0 +1,206 @@
+package de.ait.smallBusiness_be.purchases.controllers.api;
+
+
+import de.ait.smallBusiness_be.customers.dto.CustomerDto;
+import de.ait.smallBusiness_be.exceptions.ErrorResponseDto;
+import de.ait.smallBusiness_be.purchases.dto.NewPurchaseDto;
+import de.ait.smallBusiness_be.purchases.dto.PurchaseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.tags.Tags;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+@Tags(
+        @Tag(name = "Purchases")
+)
+@RequestMapping("/api/purchases")
+public interface PurchasesApi {
+    //@PreAuthorize("isAuthenticated()")
+    @PostMapping
+    @Operation(
+            summary = "Add a new purchase",
+            description = "Create a new purchase. Admin is allowed.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",
+                    description = "Purchase created successfully.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PurchaseDto.class))),
+            @ApiResponse(responseCode = "400",
+                    description = "Invalid purchase data.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class))),
+
+            @ApiResponse(responseCode = "401",
+                    description = "User unauthorized.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(type = "string"))),
+            @ApiResponse(responseCode = "403",
+                    description = "Forbidden.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(type = "string")))
+    })
+    @ResponseStatus(HttpStatus.CREATED)
+    PurchaseDto createPurchase(
+            @RequestBody @Valid NewPurchaseDto newPurchaseDto);
+
+    //@PreAuthorize("isAuthenticated()")
+    @GetMapping
+    @Operation(
+            summary = "Get all purchases",
+            description = "Retrieve a list of all purchases.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "List of Purchases retrieved successfully.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PurchaseDto[].class))),
+            @ApiResponse(responseCode = "404",
+                    description = "No purchases found.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(responseCode = "401",
+                    description = "User unauthorized.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(type = "string")))
+    })
+    @ResponseStatus(HttpStatus.OK)
+    Page<PurchaseDto> getAllPurchases(
+            @PageableDefault(size = 10) Pageable pageable,
+            @RequestParam(defaultValue = "name") String sort); // сделать сортировку по дате
+
+    @GetMapping("/{id}")
+    @Operation(
+            summary = "Get purchase by ID",
+            description = "Retrieve a purchase by its ID. Allowed to all users.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Purchase retrieved successfully.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PurchaseDto.class))),
+            @ApiResponse(responseCode = "404",
+                    description = "purchase not found.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
+    @ResponseStatus(HttpStatus.OK)
+    PurchaseDto getPurchaseById(@PathVariable Long id);
+
+    //@PreAuthorize("isAuthenticated()")
+    @GetMapping("/search/{query}")
+    @Operation(
+            summary = "Get all purchases according to search parameters",
+            description = "Retrieve a list of all purchases.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "List of Purchases retrieved successfully.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PurchaseDto[].class))),
+            @ApiResponse(responseCode = "404",
+                    description = "No purchases found.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(responseCode = "401",
+                    description = "User unauthorized.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(type = "string")))
+    })
+    @ResponseStatus(HttpStatus.OK)
+    Page<PurchaseDto> searchPurchases(
+            @PageableDefault(size = 10) Pageable pageable,
+            @RequestParam(defaultValue = "name") String sort,
+            @PathVariable String query);
+
+    //@PreAuthorize("isAuthenticated()")
+    @GetMapping("/filter")
+    @Operation(
+            summary = "Get all purchases according to the filters",
+            description = "Retrieve a list of all purchases.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "List of Purchases retrieved successfully.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = CustomerDto[].class))),
+            @ApiResponse(responseCode = "404",
+                    description = "No purchases found.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(responseCode = "401",
+                    description = "User unauthorized.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(type = "string")))
+    })
+    @ResponseStatus(HttpStatus.OK)
+    Page<PurchaseDto> getAllPurchasesByFilter(
+            @PageableDefault(size = 10) Pageable pageable,
+            @RequestParam(defaultValue = "name") String sort,
+            @RequestParam(required = false) Long id,
+            @RequestParam(required = false) Long vendorId,
+            @RequestParam(required = false) String document,
+            @RequestParam(required = false) String documentNumber,
+            @RequestParam(required = false) BigDecimal total,
+            @RequestParam(required = false) String paymentStatus);
+
+    //@PreAuthorize("isAuthenticated()")
+    @PutMapping("/{id}")
+    @Operation(
+            summary = "Update the purchase",
+            description = "Update the purchase. Admin is allowed.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Purchase updated successfully.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PurchaseDto.class))),
+            @ApiResponse(responseCode = "400",
+                    description = "Invalid purchase data.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(responseCode = "401",
+                    description = "User unauthorized.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(type = "string"))),
+            @ApiResponse(responseCode = "403",
+                    description = "Forbidden.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(type = "string")))
+    })
+    @ResponseStatus(HttpStatus.OK)
+    PurchaseDto updatePurchase(
+            @PathVariable Long id,
+            @RequestBody @Valid NewPurchaseDto newPurchaseDto);
+
+    //@PreAuthorize("isAuthenticated()")
+    @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Delete purchase by ID",
+            description = "Delete an existing purchase. Admin is allowed.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",
+                    description = "Purchase deleted successfully."),
+            @ApiResponse(responseCode = "401",
+                    description = "User unauthorized.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = String.class))),
+            @ApiResponse(responseCode = "403",
+                    description = "Forbidden.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(responseCode = "404",
+                    description = "Purchase not found.",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void deletePurchase(
+            @PathVariable Long id);
+
+}
