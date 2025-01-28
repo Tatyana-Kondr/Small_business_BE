015a91017231747a36edfa24b1237a2f2caebd48
GalinaChechkina <galina.chechkina@gmail.com>
2025-01-28 21:06:30 +0100

    added OpenAPI, cors, PasswordEncoder

--- src/main/java/de/ait/smallBusiness_be/configuration/ServiceConfiguration.java
+++ src/main/java/de/ait/smallBusiness_be/configuration/ServiceConfiguration.java
@@ -4,10 +4,26 @@ package de.ait.smallBusiness_be.configuration;
 import de.ait.smallBusiness_be.customers.dto.CustomerDto;
 import de.ait.smallBusiness_be.customers.dto.NewCustomerDto;
 import de.ait.smallBusiness_be.customers.model.Customer;
+import de.ait.smallBusiness_be.users.dto.StandardResponseDto;
+import de.ait.smallBusiness_be.documentation.OpenApiDocumentation;
+import io.swagger.v3.core.converter.AnnotatedType;
+import io.swagger.v3.core.converter.ModelConverters;
+import io.swagger.v3.core.converter.ResolvedSchema;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 /**
  * SmallBusiness_BE
@@ -38,5 +54,40 @@ public class ServiceConfiguration {
         modelMapper.createTypeMap(NewCustomerDto.class, Customer.class)
                 .addMapping(NewCustomerDto::getAddressDto, Customer::setAddress);
         return modelMapper;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public OpenAPI openAPI(@Value("${base.url:http://localhost:8080}") String baseUrl) {
+        ResolvedSchema resolvedSchema = ModelConverters.getInstance()
+                .resolveAsResolvedSchema(
+                        new AnnotatedType(StandardResponseDto.class).resolveAsRef(false));
+
+        return new OpenAPI()
+                .servers(Arrays.asList(
+                        new Server().url("http://localhost:8080"),
+                        new Server().url(baseUrl)
+                ))
+                .components(new Components()
+                        .addSchemas("EmailAndPassword", OpenApiDocumentation.emailAndPassword())
+                        .addSecuritySchemes("cookieAuth", OpenApiDocumentation.securityScheme())
+                        .addSchemas("StandardResponseDto", resolvedSchema.schema.description("StandardResponseDto")))
+                .addSecurityItem(OpenApiDocumentation.buildSecurity())
+                .paths(OpenApiDocumentation.buildAuthenticationPath())
+                .info(new Info().title("Small_ business_BE Project API").version("0.1"));
+    }
+
+    @Bean
+    public WebMvcConfigurer cors() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
