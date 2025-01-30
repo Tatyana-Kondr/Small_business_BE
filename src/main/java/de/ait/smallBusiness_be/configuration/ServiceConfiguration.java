7dc63f9436f7c0491f54c349b969313915903bf8
GalinaChechkina <galina.chechkina@gmail.com>
2025-01-30 22:18:49 +0100

    deleted html-form for login

--- src/main/java/de/ait/smallBusiness_be/configuration/ServiceConfiguration.java
+++ src/main/java/de/ait/smallBusiness_be/configuration/ServiceConfiguration.java
@@ -4,16 +4,6 @@ package de.ait.smallBusiness_be.configuration;
 import de.ait.smallBusiness_be.customers.dto.CustomerDto;
 import de.ait.smallBusiness_be.customers.dto.NewCustomerDto;
 import de.ait.smallBusiness_be.customers.model.Customer;
-import de.ait.smallBusiness_be.users.dto.StandardResponseDto;
-import de.ait.smallBusiness_be.documentation.OpenApiDocumentation;
-import io.swagger.v3.core.converter.AnnotatedType;
-import io.swagger.v3.core.converter.ModelConverters;
-import io.swagger.v3.core.converter.ResolvedSchema;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
@@ -23,7 +13,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Arrays;
 
 /**
  * SmallBusiness_BE
@@ -59,26 +48,6 @@ public class ServiceConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public OpenAPI openAPI(@Value("${base.url:http://localhost:8080}") String baseUrl) {
-        ResolvedSchema resolvedSchema = ModelConverters.getInstance()
-                .resolveAsResolvedSchema(
-                        new AnnotatedType(StandardResponseDto.class).resolveAsRef(false));
-
-        return new OpenAPI()
-                .servers(Arrays.asList(
-                        new Server().url("http://localhost:8080"),
-                        new Server().url(baseUrl)
-                ))
-                .components(new Components()
-                        .addSchemas("EmailAndPassword", OpenApiDocumentation.emailAndPassword())
-                        .addSecuritySchemes("cookieAuth", OpenApiDocumentation.securityScheme())
-                        .addSchemas("StandardResponseDto", resolvedSchema.schema.description("StandardResponseDto")))
-                .addSecurityItem(OpenApiDocumentation.buildSecurity())
-                .paths(OpenApiDocumentation.buildAuthenticationPath())
-                .info(new Info().title("Small_ business_BE Project API").version("0.1"));
     }
 
     @Bean
