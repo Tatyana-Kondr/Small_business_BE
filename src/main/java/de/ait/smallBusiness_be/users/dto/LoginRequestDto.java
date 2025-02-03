0d0cc7615454bcda623c8c239b8860c3e7866d50
GalinaChechkina <galina.chechkina@gmail.com>
2025-02-03 14:37:49 +0100

    changed schema

--- src/main/java/de/ait/smallBusiness_be/users/dto/LoginRequestDto.java
+++ src/main/java/de/ait/smallBusiness_be/users/dto/LoginRequestDto.java
@@ -4,10 +4,10 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "DTO representing login request with user credentials")
 public record LoginRequestDto(
-        @Schema(description = "Email of the user", example = "admin@gmail.de")
+        @Schema(description = "Email of the user", example = "user@gmail.com")
         String email,
 
-        @Schema(description = "Password of the user", example = "Password007!")
+        @Schema(description = "Password of the user", example = "Qwerty007!")
         String password
 ) {
 }
