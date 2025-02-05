5a1599db01c6697dd2cdb02db489372f31f13110
GalinaChechkina <galina.chechkina@gmail.com>
2025-02-05 12:48:00 +0100

    created models: Sale, SaleItem

---
+++ src/main/java/de/ait/smallBusiness_be/sales/models/Sale.java
@@ -0,0 +1,71 @@
+package de.ait.smallBusiness_be.sales.models;
+
+import de.ait.smallBusiness_be.products.model.Dimensions;
+import de.ait.smallBusiness_be.purchases.model.PaymentStatus;
+import de.ait.smallBusiness_be.purchases.model.TypeOfOperation;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 2/5/2025
+ * Small_business_BE
+ *
+ * @author Chechkina (AIT TR)
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@Entity
+@Table(name = "Sales")
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long customerId;
+
+    @Column(nullable = false)
+    private String invoiceNumber;
+
+    @Column
+    private String accountObject; // в некоторых счетах необходимо указывать объекты
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TypeOfOperation typeOfOperation;
+
+    @Column
+    private String shipping;  // доставка, здесь будут фиксированные значения
+
+    @Embedded
+    private Dimensions shippingDimensions; //габариты посылки (вес, размер)
+
+    @Column
+    private String termsOfPayment; // условия оплаты????
+
+    @Column
+    @PastOrPresent
+    private LocalDate salesDate;
+
+    @Column
+    @PastOrPresent
+    private LocalDate paymentDate;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
+    @OneToMany(mappedBy = "Sale", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<SaleItem> salesItems = new ArrayList<>();
+}
---
+++ src/main/java/de/ait/smallBusiness_be/sales/models/SaleItem.java
@@ -0,0 +1,78 @@
+package de.ait.smallBusiness_be.sales.models;
+
+import de.ait.smallBusiness_be.products.model.Product;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+/**
+ * 2/5/2025
+ * Small_business_BE
+ *
+ * @author Chechkina (AIT TR)
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@Entity
+@Table(name = "SaleItems")
+public class SaleItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer position;
+
+    @ManyToOne
+    @JoinColumn(name = "sale_id", referencedColumnName = "id", nullable = false)
+    @NotNull(message = "{validation.notNull}")
+    private Sale sale;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id", nullable = false)
+    @NotNull(message = "{validation.notNull}")
+    private Product product;
+
+    @Column(nullable = false)
+    private String productName;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(precision = 8, scale = 2)
+    @DecimalMin(value = "0.0", message = "{validation.price.min}")
+    @Digits(integer = 6, fraction = 2, message = "{validation.price.digits}")
+    private BigDecimal unitPrice;
+
+    @Column(precision = 2)
+    @DecimalMin(value = "0", message = "{validation.tax.min}")
+    private BigDecimal discount;
+
+    @Column(precision = 8, scale = 2)
+    @DecimalMin(value = "0.0", message = "{validation.price.min}")
+    @Digits(integer = 6, fraction = 2, message = "{validation.price.digits}")
+    private BigDecimal totalPrice;
+
+    @Column(precision = 2)
+    @DecimalMin(value = "0", message = "{validation.tax.min}")
+    private BigDecimal tax;
+
+    @Column(precision = 8, scale = 2)
+    @DecimalMin(value = "0.0", message = "{validation.price.min}")
+    @Digits(integer = 6, fraction = 2, message = "{validation.price.digits}")
+    private BigDecimal taxAmount;
+
+    @Column(precision = 8, scale = 2)
+    @DecimalMin(value = "0.0", message = "{validation.price.min}")
+    @Digits(integer = 6, fraction = 2, message = "{validation.price.digits}")
+    private BigDecimal totalAmount;
+}
