d9b21730e4a011bad330f18fd74cdc0cd3f8c05b
GalinaChechkina <galina.chechkina@gmail.com>
2025-02-07 13:59:06 +0100

    added findAllBySaleIdOrderByPosition

--- src/main/java/de/ait/smallBusiness_be/sales/dao/SaleItemRepository.java
+++ src/main/java/de/ait/smallBusiness_be/sales/dao/SaleItemRepository.java
@@ -4,7 +4,11 @@ import de.ait.smallBusiness_be.sales.models.SaleItem;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SaleItemRepository extends CrudRepository<SaleItem, Long> {
+
+    List <SaleItem> findAllBySaleIdOrderByPosition(Long saleId);
 
 }
