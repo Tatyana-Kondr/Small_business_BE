bdb1e233fa1090739803c356e1796d8f0a64f615
GalinaChechkina <galina.chechkina@gmail.com>
2025-02-07 14:03:37 +0100

    created checkIfSaleExistsById, createSale

--- src/main/java/de/ait/smallBusiness_be/sales/services/impl/SaleServiceImpl.java
+++ src/main/java/de/ait/smallBusiness_be/sales/services/impl/SaleServiceImpl.java
@@ -1,23 +1,27 @@
 package de.ait.smallBusiness_be.sales.services.impl;
 
-import de.ait.smallBusiness_be.customers.dao.CustomerRepository;
 import de.ait.smallBusiness_be.customers.model.Customer;
+import de.ait.smallBusiness_be.customers.services.CustomerService;
 import de.ait.smallBusiness_be.exceptions.ErrorDescription;
 import de.ait.smallBusiness_be.exceptions.RestApiException;
-import de.ait.smallBusiness_be.products.dao.ProductRepository;
+import de.ait.smallBusiness_be.products.model.Dimensions;
+import de.ait.smallBusiness_be.products.model.Product;
+import de.ait.smallBusiness_be.products.service.ProductService;
 import de.ait.smallBusiness_be.sales.dao.SaleRepository;
 import de.ait.smallBusiness_be.sales.dto.NewSaleDto;
 import de.ait.smallBusiness_be.sales.dto.SaleDto;
 import de.ait.smallBusiness_be.sales.models.Sale;
+import de.ait.smallBusiness_be.sales.models.SaleItem;
 import de.ait.smallBusiness_be.sales.services.SaleService;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.mapper.Mapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * 2/5/2025
@@ -30,19 +34,39 @@ import org.springframework.transaction.annotation.Transactional;
 public class SaleServiceImpl implements SaleService {
 
     private final SaleRepository saleRepository;
-    private final CustomerRepository customerRepository;
-    private final ProductRepository productRepository;
+
+    private final CustomerService customerService;
+    private final ProductService productService;
     private final ModelMapper modelMapper;
 
     @Override
     @Transactional
     public SaleDto createSale(NewSaleDto newSale) {
 
-        Customer customer = customerRepository
-                .findById(newSale.customerId())
-                .orElseThrow(()-> new RestApiException(ErrorDescription.CUSTOMER_NOT_FOUND));
+        Customer customer = customerService.getCustomerOrThrow(newSale.customerId());
 
-        return null;
+        Sale sale = modelMapper.map(newSale, Sale.class);
+        sale.setCustomer(customer);
+        sale.setShippingDimensions(newSale.shippingDimensions() != null ?
+                modelMapper.map(newSale.shippingDimensions(), Dimensions.class) :
+                null);
+
+        if (newSale.salesItems()!=null && !newSale.salesItems().isEmpty()) {
+
+            List<SaleItem> saleItems = newSale.salesItems().stream()
+                    .map(newSaleItemDto -> {
+                        Product product = productService.getProductOrThrow(newSaleItemDto.productId());
+                        SaleItem saleItem = modelMapper.map(newSaleItemDto, SaleItem.class);
+                        saleItem.setProduct(product);
+                        saleItem.setSale(sale);
+                        return saleItem;
+                    }).toList();
+            sale.setSalesItems(saleItems);
+        }
+
+        Sale savedSale = saleRepository.save(sale);
+
+        return modelMapper.map(savedSale, SaleDto.class);
     }
 
     @Override
@@ -72,5 +96,11 @@ public class SaleServiceImpl implements SaleService {
         return saleRepository
                 .findById(id)
                 .orElseThrow(() -> new RestApiException(ErrorDescription.SALE_NOT_FOUND, HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    public boolean checkIfSaleExistsById(Long saleId) {
+
+        return saleRepository.existsById(saleId);
     }
 }
