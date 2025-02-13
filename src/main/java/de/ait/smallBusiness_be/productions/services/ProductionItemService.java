78edc79e542841ae7115de3b4af81ef187e5bc4f
Tatyana Kondratyeva <tatyana.k@gmx.net>
2025-02-13 20:43:26 +0100

    fixed ProductionItemService

--- src/main/java/de/ait/smallBusiness_be/productions/services/ProductionItemService.java
+++ src/main/java/de/ait/smallBusiness_be/productions/services/ProductionItemService.java
@@ -2,12 +2,10 @@ package de.ait.smallBusiness_be.productions.services;
 
 import de.ait.smallBusiness_be.productions.dto.NewProductionItemDto;
 import de.ait.smallBusiness_be.productions.dto.ProductionItemDto;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+
 
 public interface ProductionItemService {
-    ProductionItemDto createProductionItem(NewProductionItemDto newProductionItemDto);
-    Page<ProductionItemDto> getAllProductionItems(Pageable pageable);
+    ProductionItemDto createProductionItem(NewProductionItemDto newProductionItemDto, Long productionId);
     ProductionItemDto getProductionItemById(Long id);
     ProductionItemDto updateProductionItem(Long id, NewProductionItemDto newProductionItemDto);
     void deleteProductionItem(Long id);
