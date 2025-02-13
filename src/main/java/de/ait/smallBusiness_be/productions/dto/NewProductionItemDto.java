1ad5e0dbef1343e1b4bf453e13d6fb3a1fb182e7
Tatyana Kondratyeva <tatyana.k@gmx.net>
2025-02-13 11:06:15 +0100

    added ProductionDto, ProductionItemDto, NewProductionDto, NewProductionItemDto

---
+++ src/main/java/de/ait/smallBusiness_be/productions/dto/NewProductionItemDto.java
@@ -0,0 +1,50 @@
+package de.ait.smallBusiness_be.productions.dto;
+
+import de.ait.smallBusiness_be.products.model.Product;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+/**
+ * 12.02.2025
+ * SmB_be
+ *
+ * @author Kondratyeva (AIT TR)
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(name = "New ProductionItem", description = "Data for registration of new productionItem")
+public class NewProductionItemDto {
+
+    @NotNull(message = "{validation.notNull}")
+    @Schema(description = "Product's id", example = "1")
+    Long productId;
+
+    @Schema(description = "Type of operation", example = "PRODUKTIONSMATERIAL")
+    String type;
+
+    @NotNull(message = "{validation.notNull}")
+    @Schema(description = "Quantity", example = "1")
+    Integer quantity;
+
+    @DecimalMin(value = "0.0", message = "{validation.price.min}")
+    @Digits(integer = 6, fraction = 2, message = "{validation.price.digits}")
+    @Schema(description = "Unit price", example = "5.00")
+    BigDecimal unitPrice;
+
+    @NotNull(message = "{validation.notNull}")
+    @DecimalMin(value = "0.0", message = "{validation.price.min}")
+    @Digits(integer = 6, fraction = 2, message = "{validation.price.digits}")
+    @Schema(description = "Amount without percentage", example = "0")
+    BigDecimal totalPrice;
+}
---
+++ src/main/java/de/ait/smallBusiness_be/productions/dto/ProductionItemDto.java
@@ -0,0 +1,33 @@
+package de.ait.smallBusiness_be.productions.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+/**
+ * 13.02.2025
+ * SmB_be
+ *
+ * @author Kondratyeva (AIT TR)
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(name = "ProductionItem", description = "Data of productionItem")
+public class ProductionItemDto {
+
+    Long productId;
+    String type;
+    Integer quantity;
+    BigDecimal unitPrice;
+    BigDecimal totalPrice;
+}
