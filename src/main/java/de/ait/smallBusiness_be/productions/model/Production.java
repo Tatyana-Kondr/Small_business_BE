5bf26ad08d258a8ff8241e091753e99d74a41fb7
Tatyana Kondratyeva <tatyana.k@gmx.net>
2025-02-13 11:06:50 +0100

    added models Production and ProductionItem

---
+++ src/main/java/de/ait/smallBusiness_be/productions/model/Production.java
@@ -0,0 +1,65 @@
+package de.ait.smallBusiness_be.productions.model;
+import de.ait.smallBusiness_be.products.model.Product;
+import de.ait.smallBusiness_be.purchases.model.TypeOfOperation;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 12.02.2025
+ * SmB_be
+ *
+ * @author Kondratyeva (AIT TR)
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@Entity
+@Table(name = "production")
+public class Production {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column
+    @PastOrPresent(message = "{validation.dateOfLastPurchase.pastOrPresent}")
+    LocalDate dateOfProduction;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    TypeOfOperation type;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id", nullable = false)
+    @NotNull(message = "{validation.notNull}")
+    Product product;
+
+    @Column(nullable = false)
+    Integer quantity;
+
+    @Column(precision = 8, scale = 2)
+    @DecimalMin(value = "0.0", message = "{validation.price.min}")
+    @Digits(integer = 6, fraction = 2, message = "{validation.price.digits}")
+    BigDecimal unitPrice;
+
+    @Column(precision = 8, scale = 2)
+    @DecimalMin(value = "0.0", message = "{validation.price.min}")
+    @Digits(integer = 6, fraction = 2, message = "{validation.price.digits}")
+    BigDecimal amount;
+
+    @OneToMany(mappedBy = "purchase", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    List<ProductionItem> purchaseItems = new ArrayList<>();
+}
---
+++ src/main/java/de/ait/smallBusiness_be/productions/model/ProductionItem.java
@@ -0,0 +1,53 @@
+package de.ait.smallBusiness_be.productions.model;
+
+import de.ait.smallBusiness_be.products.model.Product;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+/**
+ * 12.02.2025
+ * SmB_be
+ *
+ * @author Kondratyeva (AIT TR)
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@Entity
+@Table(name = "production_items")
+public class ProductionItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id", nullable = false)
+    @NotNull(message = "{validation.notNull}")
+    Product product;
+
+    @ManyToOne
+    @JoinColumn(name = "production_id", referencedColumnName = "id", nullable = false)
+    @NotNull(message = "{validation.notNull}")
+    Production production;
+
+    @Column(nullable = false)
+    Integer quantity;
+
+    @Column(precision = 8, scale = 2)
+    @DecimalMin(value = "0.0", message = "{validation.price.min}")
+    @Digits(integer = 6, fraction = 2, message = "{validation.price.digits}")
+    BigDecimal unitPrice;
+
+    @Column(precision = 8, scale = 2)
+    @DecimalMin(value = "0.0", message = "{validation.price.min}")
+    @Digits(integer = 6, fraction = 2, message = "{validation.price.digits}")
+    BigDecimal totalPrice;// Общая стоимость.
+}
