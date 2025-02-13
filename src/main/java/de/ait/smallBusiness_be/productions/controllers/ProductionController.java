bfd05a6f4521a3298ccd9022a9c6d6e196703e94
Tatyana Kondratyeva <tatyana.k@gmx.net>
2025-02-13 20:38:07 +0100

    added all methods

--- src/main/java/de/ait/smallBusiness_be/productions/controllers/ProductionController.java
+++ src/main/java/de/ait/smallBusiness_be/productions/controllers/ProductionController.java
@@ -4,8 +4,10 @@ import de.ait.smallBusiness_be.productions.controllers.api.ProductionsApi;
 import de.ait.smallBusiness_be.productions.dto.NewProductionDto;
 import de.ait.smallBusiness_be.productions.dto.ProductionDto;
 import de.ait.smallBusiness_be.productions.services.ProductionService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 13.02.2025
@@ -13,30 +15,35 @@ import org.springframework.data.domain.Pageable;
  *
  * @author Kondratyeva (AIT TR)
  */
+
+@RestController
+@RequiredArgsConstructor
 public class ProductionController implements ProductionsApi {
+
+    private final ProductionService productionService;
 
     @Override
     public ProductionDto addProduction(NewProductionDto newProductionDto) {
-        return null;
+        return productionService.createProduction(newProductionDto);
     }
 
     @Override
     public Page<ProductionDto> getAllProductions(Pageable pageable) {
-        return null;
+        return productionService.getAllProductions(pageable);
     }
 
     @Override
     public ProductionDto getProductionById(Long id) {
-        return null;
+        return productionService.getProductionById(id);
     }
 
     @Override
     public ProductionDto updateProduction(Long id, NewProductionDto newProductionDto) {
-        return null;
+        return productionService.updateProduction(id, newProductionDto);
     }
 
     @Override
-    public void removePurchase(Long id) {
-
+    public void removeProduction(Long id) {
+        productionService.deleteProduction(id);
     }
 }
