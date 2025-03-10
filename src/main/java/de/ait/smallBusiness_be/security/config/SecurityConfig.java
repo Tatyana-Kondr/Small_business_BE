3f284c8cb5f851ffd5410334b2147b080e2c6af5
tatya <tatyana.k@gmx.net>
2025-03-10 12:57:26 +0100

    fixed hasAuthority("ADMIN") for customer

--- src/main/java/de/ait/smallBusiness_be/security/config/SecurityConfig.java
+++ src/main/java/de/ait/smallBusiness_be/security/config/SecurityConfig.java
@@ -48,9 +48,9 @@ public class SecurityConfig {
                         .requestMatchers(AUTH_WHITELIST).permitAll() // Swagger доступен всем
                         .requestMatchers(HttpMethod.POST, "/api/users/register","/api/auth/login", "/api/auth/logout").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/products", "/api/products/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/product-categories").hasAuthority("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/api/product-categories", "/api/purchases/{id}", "/api/productions/{id}").hasAuthority("ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/api/product-categories", "/api/products/{id}", "/api/purchaseItems{id}", "/api/productions/{id}").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/product-categories", "/api/customers").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/product-categories", "/api/purchases/{id}", "/api/productions/{id}", "/api/customers/{id}").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/product-categories/{id}", "/api/products/{id}", "/api/purchaseItems{id}", "/api/productions/{id}", "/api/customers/{id}").hasAuthority("ADMIN")
 //                        .requestMatchers(HttpMethod.GET, "/api/products/**", "/api/product-categories/**").permitAll()
 //                        .requestMatchers(HttpMethod.POST, "/api/products", "/api/product-categories", "/api/customers").permitAll()
 //                        .requestMatchers(HttpMethod.PUT, "/api/products/{id}", "/api/product-categories/{id}").permitAll()
