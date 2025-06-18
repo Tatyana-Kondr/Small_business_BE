0ca9a05edb9f082638f20c1a2122319d0b07c167
tatya <tatyana.k@gmx.net>
2025-06-18 15:42:39 +0200

    fixed SaleItemRepository

--- src/main/java/de/ait/smallBusiness_be/sales/dao/SaleItemRepository.java
+++ src/main/java/de/ait/smallBusiness_be/sales/dao/SaleItemRepository.java
@@ -1,17 +1,17 @@
 package de.ait.smallBusiness_be.sales.dao;
 
 import de.ait.smallBusiness_be.sales.models.SaleItem;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface SaleItemRepository extends CrudRepository<SaleItem, Long> {
+public interface SaleItemRepository extends JpaRepository<SaleItem, Long> {
 
-    List <SaleItem> findAllBySaleIdOrderByPosition(Long saleId);
+    List<SaleItem> findAllBySaleIdOrderByPosition(Long saleId);
 
     @Query("SELECT COALESCE(MAX(s.position), 0) FROM SaleItem s WHERE s.sale.id = :saleId")
     Integer findMaxPositionBySaleId(@Param("saleId")Long saleId);
