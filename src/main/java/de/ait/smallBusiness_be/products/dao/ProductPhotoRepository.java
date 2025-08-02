397dcf0d2a70b0859d966f282a598ab94f8c8f4d
tatya <tatyana.k@gmx.net>
2025-08-02 10:39:51 +0200

    added @Repository

--- src/main/java/de/ait/smallBusiness_be/products/dao/ProductPhotoRepository.java
+++ src/main/java/de/ait/smallBusiness_be/products/dao/ProductPhotoRepository.java
@@ -2,9 +2,11 @@ package de.ait.smallBusiness_be.products.dao;
 
 import de.ait.smallBusiness_be.products.model.ProductPhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProductPhotoRepository extends JpaRepository<ProductPhoto, Long> {
 
     List<ProductPhoto> findByProductId(Long productId);
