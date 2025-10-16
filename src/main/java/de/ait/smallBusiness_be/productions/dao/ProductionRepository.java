8d1efd6aabf531f383c556c7ff42fe984790c504
tatya <tatyana.k@gmx.net>
2025-10-16 13:47:57 +0200

    fixed ProductionRepository

--- src/main/java/de/ait/smallBusiness_be/productions/dao/ProductionRepository.java
+++ src/main/java/de/ait/smallBusiness_be/productions/dao/ProductionRepository.java
@@ -2,7 +2,7 @@ package de.ait.smallBusiness_be.productions.dao;
 
 import de.ait.smallBusiness_be.productions.model.Production;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 /**
  * 13.02.2025
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Repository;
  * @author Kondratyeva (AIT TR)
  */
 
-@Repository
-public interface ProductionRepository extends JpaRepository<Production, Long> {
+
+public interface ProductionRepository extends JpaRepository<Production, Long>, ProductionRepositoryCustom {
+
 }
