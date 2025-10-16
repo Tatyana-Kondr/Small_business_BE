b583132f843bf8dc8b26cc26f990e8397c975cf3
tatya <tatyana.k@gmx.net>
2025-10-16 13:50:03 +0200

    added methods getProductionsByFilter and searchProductions

--- src/main/java/de/ait/smallBusiness_be/productions/services/ProductionService.java
+++ src/main/java/de/ait/smallBusiness_be/productions/services/ProductionService.java
@@ -2,8 +2,12 @@ package de.ait.smallBusiness_be.productions.services;
 
 import de.ait.smallBusiness_be.productions.dto.NewProductionDto;
 import de.ait.smallBusiness_be.productions.dto.ProductionDto;
+import de.ait.smallBusiness_be.purchases.dto.PurchaseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 public interface ProductionService {
 
@@ -12,4 +16,6 @@ public interface ProductionService {
     ProductionDto getProductionById(Long id);
     ProductionDto updateProduction(Long id, NewProductionDto newProductionDto);
     void deleteProduction(Long id);
+    Page<ProductionDto> searchProduction(Pageable pageable, String query);
+    Page<ProductionDto> getAllProductionsByFilter(Pageable pageable, LocalDate startDate, LocalDate endDate, String searchQuery);
 }
