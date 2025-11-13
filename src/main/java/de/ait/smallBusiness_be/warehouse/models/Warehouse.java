e627343892536336ce5dc8f89c1bd54bf74cf0a8
tatya <tatyana.k@gmx.net>
2025-11-13 20:27:00 +0100

    created Warehouse

---
+++ src/main/java/de/ait/smallBusiness_be/warehouse/models/Warehouse.java
@@ -0,0 +1,34 @@
+package de.ait.smallBusiness_be.warehouse.models;
+
+import de.ait.smallBusiness_be.products.model.Product;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "warehouse")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @DecimalMin(value = "0.0", message = "{validation.price.min}")
+    @Digits(integer = 6, fraction = 3, message = "{validation.price.digits}")
+    private BigDecimal quantity;
+}
+
