de4867e5298ef9d60df91fca487141e57d0b9291
tatya <tatyana.k@gmx.net>
2025-11-17 21:01:55 +0100

    created WarehouseRecordRepository

--- src/main/java/de/ait/smallBusiness_be/warehouse/dao/WarehouseRecordRepository.java
+++ src/main/java/de/ait/smallBusiness_be/warehouse/dao/WarehouseRecordRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface WarehouseRecordRepository extends JpaRepository<WarehouseRecord, Long> {
     Page<WarehouseRecord> findAllByProductIdOrderByDateDesc(Long productId, Pageable pageable);
+    List<WarehouseRecord> findAllByDocumentId(Long documentId);
+    void deleteAllByDocumentId(Long documentId);
 }
